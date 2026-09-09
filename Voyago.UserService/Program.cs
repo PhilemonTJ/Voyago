12e1c0c9efe102556097e2443a72933525be88b8
Philemon Joseph <philemon.joseph@dataart.com>
2026-09-09 18:57:54 +0530

    Revert "feat(user): add user registration and login"
    This reverts commit 7775ecb842b18c6d851e2f8e0d6c97b1450e029d.

--- Voyago.UserService/Program.cs
+++ Voyago.UserService/Program.cs
@@ -1,8 +1,4 @@
-using Microsoft.AspNetCore.Identity;
-using System.Text.Json.Serialization;
 using Voyago.UserService.Data;
-using Voyago.UserService.Models;
-using Voyago.UserService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,20 +7,7 @@ builder.AddServiceDefaults();
 builder.AddNpgsqlDbContext<UserDbContext>(
     connectionName: "userdb");
 
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
-
-builder.Services
-    .AddControllers()
-    .AddJsonOptions(options  =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
-
 builder.Services.AddOpenApi();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -33,13 +16,8 @@ app.MapDefaultEndpoints();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.MapControllers();
 
 app.Run();
